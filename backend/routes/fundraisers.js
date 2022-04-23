31e2db1eec421f65c62b22dc56e4ed0f4fcfc702
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2022-04-23 13:28:22 +0200

    create /fundraisers GET API call

--- backend/routes/fundraisers.js
+++ backend/routes/fundraisers.js
@@ -5,8 +5,53 @@ const db = require('better-sqlite3')('database.db');
 
 router
   .get('/', function(req, res){
-  	const rows = db.prepare("SELECT * FROM fundraisers").all();
-  	res.json({"success": true, "fundraisers": rows});
+  	// TODO: get user id => flag if user subscribed  
+
+  	// Better use eye drops before reading this code :)
+  	const fundraisers = db.prepare(`select f.*, 
+sum(r.current_quantity) as current_quantity, sum(r.required_quantity) as required_quantity
+from fundraisers as f 
+left join requirements as r
+on r.fundraiser_id = f.fundraiser_id
+group by f.fundraiser_id`).all();
+  	let ids = [];
+  	for (let rowId in fundraisers) {
+  		let row = fundraisers[rowId];
+  		const requirements = db.prepare(`select r.*, p.name as product_name, pp.name as category_name, pp.product_id as category_id
+from requirements as r
+inner join products as p
+on p.product_id = r.product_id
+left join products as pp 
+on pp.product_id = p.parent_id
+where r.fundraiser_id = ?`).all(row.fundraiser_id);
+
+  		let categories = {};
+  		for (let req of requirements) {
+  			if (!(req.category_id in categories)) {
+  				categories[req.category_id] = {
+  					category_name: req.category_name,
+  					priority: req.priority,
+  					category_id: req.category_id
+  				};
+  			}
+  			categories[req.category_id].priority = Math.max(
+  				categories[req.category_id].priority,
+  				req.priority
+  			);
+  		}
+
+  		let categoriesList = [];
+  		for (let key in categories) {
+  			categoriesList.push(categories[key]);
+  		}
+
+  		row.requirements = requirements;
+  		row.categories = categoriesList;
+  		fundraisers[rowId] = row;
+
+  	}
+
+  	res.json({"success": true, "fundraisers": fundraisers});
   });
 
  
