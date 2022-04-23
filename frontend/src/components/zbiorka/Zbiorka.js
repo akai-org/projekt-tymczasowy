a9f752cceb8e235f8f414fffa8c64a0c1e5581f2
AleksandraGorska02 <aleksandra.gorska02@gmail.com>
2022-04-23 13:06:52 +0200

    zbiorka

--- frontend/src/components/zbiorka/Zbiorka.js
+++ frontend/src/components/zbiorka/Zbiorka.js
@@ -7,14 +7,13 @@ function Zbiorka() {
       <div class="pasek"><div class="pasek2"></div></div>
       <h2 >Potrzebne</h2>
       <div  class="box">
-      <div class="filtry">UBRANIA</div>
-      <div class="filtry">LEKI</div>
-      <div class="filtry">ZABAWKI</div>
+          <div class="filtry">UBRANIA</div>
+          <div class="filtry">LEKI</div>
+          <div class="filtry">ZABAWKI</div>
       </div>
-      <div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</p>
-      </div>
-      
+ 
+        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident</span>
+
     </div>;
 }
 
