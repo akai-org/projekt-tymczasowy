ab5737a661416493914dd6260cbde57944b6947e
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2022-04-23 16:44:21 +0200

    Navbar fixes

--- frontend/src/pages/NavBar/NavBar.js
+++ frontend/src/pages/NavBar/NavBar.js
@@ -2,18 +2,14 @@ import React from "react";
 import "./NavBar.scss";
 import Button from "../components/button/Button";
 export default function NavBar() {
-
-    const scroll = () => {
-        
-    }
+  const scroll = () => {};
 
   return (
     <div className="nav-main">
       <div className="title">Tinder dla zbórek</div>
       <div className="button-group">
-        <div className="btn">
-          <Button content="Zbiórki"></Button>
-        </div>
+        <a className="link">Zbiórki</a>
+
         <div className="btn">
           <Button content="Zaloguj się"></Button>
         </div>
