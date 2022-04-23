f846067f6ede08632cf08ba205cc92b5a6669e19
AleksandraGorska02 <aleksandra.gorska02@gmail.com>
2022-04-23 17:47:36 +0200

    opis

--- frontend/src/pages/components/opis/Opis.js
+++ frontend/src/pages/components/opis/Opis.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Opis.scss";
+import {BsArrowUpCircle } from "react-icons/bs";
 function Opis() {
   return (
     <div className="opis-main">
-      <h1 class="name">
+      <h1 class="name2">
         Zbiórka dla rodzin z Ukrainy 
       </h1>
       <span>Lokalizacja</span>
@@ -14,18 +15,114 @@ function Opis() {
       <div>
       <span>24.08.2022 </span>
       </div>  
+
+           <div class="filtry2">Główny cel</div>
+<div >
+      
+ 
+    <div class="all" > 
+ <h2 >Odzież</h2>
        <span>Osiągnięto 60% celu</span>
       <div class="pasek">
         <div class="pasek2"></div>
       </div>
    
-      <div class="filtry">Główny cel</div>
-      <h2 class="h2">Odzież</h2>
-    <div class="box-container">
-        <div class="box"></div>
-        <div class="box"></div>
-        <div class="box"></div>
+     
 
+    <div class="box-container">
+        <div class="boxxx">
+            <h3>Koszulki</h3>
+            <BsArrowUpCircle class="arrow"> </BsArrowUpCircle>
+            <span class="punkty">50pk.<p class="sztk">/1szt</p></span>
+            <div class="button2">Wspomóż</div>
+        </div>
+        <div class="boxxx">
+        <h3>Spodnie</h3>
+        <BsArrowUpCircle class="arrow"> </BsArrowUpCircle>
+            <span class="punkty">120pk.<p class="sztk">/1szt</p></span>
+            <div class="button2">Wspomóż</div>
+        </div>
+        <div class="boxxx">
+        <h3>Buty</h3>
+        <BsArrowUpCircle class="arrow"> </BsArrowUpCircle>
+        <span class="punkty">300pk.<p class="sztk">/1szt</p></span>
+            <div class="button2">Wspomóż</div>
+        </div>
+
+            </div>
+            </div>
+    
+
+
+            <div class="all">
+ <h2 >Leki</h2>
+       <span>Osiągnięto 60% celu</span>
+      <div class="pasek">
+        <div class="pasek2"></div>
+      </div>
+   
+     
+
+    <div class="box-container">
+        <div class="boxxx">
+            <h3>Przeciwbólowe</h3>
+            <BsArrowUpCircle class="arrow"> </BsArrowUpCircle>
+            <span class="punkty">500pk.<p class="sztk">/1szt</p></span>
+            <div class="button2">Wspomóż</div>
+        </div>
+        <div class="boxxx">
+        <h3>Przeciwalergiczne</h3>
+        <BsArrowUpCircle class="arrow"> </BsArrowUpCircle>
+            <span class="punkty">120pk.<p class="sztk">/1szt</p></span>
+            <div class="button2">Wspomóż</div>
+        </div>
+        <div class="boxxx">
+        <h3>Bandaże</h3>
+        <BsArrowUpCircle class="arrow"> </BsArrowUpCircle>
+        <span class="punkty">60pk.<p class="sztk">/1szt</p></span>
+            <div class="button2">Wspomóż</div>
+        </div>
+
+            </div>
+            </div>
+
+
+
+            <div class="all" >
+ <h2 >RTV i AGD</h2>
+       <span>Osiągnięto 60% celu</span>
+      <div class="pasek">
+        <div class="pasek2"></div>
+      </div>
+   
+     
+
+    <div class="box-container">
+        <div class="boxxx">
+            <h3>Mikrofalówka</h3>
+            <BsArrowUpCircle class="arrow"> </BsArrowUpCircle>
+            <span class="punkty">800pk.<p class="sztk">/1szt</p></span>
+            <div class="button2">Wspomóż</div>
+        </div>
+        <div class="boxxx">
+        <h3>Czajnik</h3>
+        <BsArrowUpCircle class="arrow"> </BsArrowUpCircle>
+            <span class="punkty">150pk.<p class="sztk">/1szt</p></span>
+            <div class="button2">Wspomóż</div>
+        </div>
+        <div class="boxxx">
+        <h3>Telefon</h3>
+        <BsArrowUpCircle class="arrow"> </BsArrowUpCircle>
+        <span class="punkty">500pk.<p class="sztk">/1szt</p></span>
+            <div class="button2">Wspomóż</div>
+        </div>
+
+            </div>
+            </div>
+    
+
+    
+    
     </div>
     </div>
   );
