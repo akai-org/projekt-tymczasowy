a1368d67c127f224d2795291dcaf76d250bae1a8
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2022-04-23 13:56:23 +0200

    Add /rewards GET api call

--- backend/routes/rewards.js
+++ backend/routes/rewards.js
@@ -1,10 +1,14 @@
 var express = require('express'),
     router = express.Router();
 
+const db = require('better-sqlite3')('database.db');
+
 router
+	// TODO: add flag user_activated based on user_id
   .get('/', function(req, res){
-  	res.send("Hello Rewards");
-  })
+  	const rewards = db.prepare(`select * from rewards`).all();
+  	res.json({"success": true, "rewards": rewards});
+  });
 
   //.use('/login', require('./users/login'))
   //.use('/register', require('./users/register'));
