846581b4a8d05fa1a384d93684bafb228bf490d7
Maciej Kaszkowiak <maciej.kaszkowiak@gmail.com>
2022-04-23 12:25:13 +0200

    add database to .gitignore, install better-sqlite3, create example api call

--- backend/routes/fundraisers.js
+++ backend/routes/fundraisers.js
@@ -1,12 +1,13 @@
 var express = require('express'),
     router = express.Router();
 
+const db = require('better-sqlite3')('database.db');
+
 router
   .get('/', function(req, res){
-  	res.send("Hello Fundraisers");
-  })
+  	const rows = db.prepare("SELECT * FROM fundraisers").all();
+  	res.json({"success": true, "fundraisers": rows});
+  });
 
-  //.use('/login', require('./users/login'))
-  //.use('/register', require('./users/register'));
  
 module.exports = router;
