e751e128674d0ca415ef7f81ad276a618d55c591
MRSIMM0 <s.pasieczny02@gmail.com>
2022-04-23 14:41:10 +0200

    changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,15 @@ import "./App.scss";
 
 import { Layout } from "./Layout";
 import { Homepage } from "./pages/Homepage/Homepage";
+import RaiseListView from "./pages/raiseListView/RaiseListView";
 
 function App() {
   return (
     <Layout>
       <Homepage />
+      <RaiseListView></RaiseListView>
     </Layout>
   );
-
 }
 
 export default App;
