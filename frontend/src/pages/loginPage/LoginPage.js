110ae500ae8a48f100f0ddf2dd499bf14491c4cd
Tobiasz Ciesielski <t.ciesielski1177@gmail.com>
2022-04-23 15:45:04 +0200

    Styles to form

--- frontend/src/pages/loginPage/LoginPage.js
+++ frontend/src/pages/loginPage/LoginPage.js
@@ -1,22 +1,31 @@
-import Input from "../components/input/Input.js"
-import "./LoginPage.scss"
+import Input from "../components/input/Input.js";
+import "./LoginPage.scss";
+
+import Button from "../components/button/Button";
 
 export default function LoginPage() {
-    return (
-        <div className="login-page-view">
-            <div className="box">
-                <div className="message">
-                    <p>Dołącz do Tinder dla zbiórek i pomagaj oddając rzeczy potrzebującym!</p>
-                </div>
-                <div className="form">
-                    <div className="input">
-                        <Input placeholder="login" />
-                    </div>
-                    <div className="input">
-                        <Input placeholder="password" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="login-page-view">
+      <div className="box">
+        <h1 className="message">
+          Dołącz do Tinder dla zbiórek i pomagaj oddając rzeczy potrzebującym!
+        </h1>
+
+        <div className="form">
+          <div className="input">
+            <Input placeholder="login" />
+          </div>
+
+          <div className="input">
+            <Input placeholder="password" />
+          </div>
         </div>
-    );
+
+        <div className="actions">
+          <Button content="Zaloguj się"></Button>
+          <a className="createAccountCTA">Stwórz konto</a>
+        </div>
+      </div>
+    </div>
+  );
 }
